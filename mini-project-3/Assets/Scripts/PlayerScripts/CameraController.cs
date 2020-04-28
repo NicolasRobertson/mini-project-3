c4bd35582f9233554acee3ece9897b9e8a89ec5e
NicolasRobertson <robe1606@morris.umn.edu>
2020-04-27 22:11:20 -0500

    Add Background
    Add Background
    Fix broken Balloon animation controller

--- mini-project-3/Assets/Scripts/PlayerScripts/CameraController.cs
+++ mini-project-3/Assets/Scripts/PlayerScripts/CameraController.cs
@@ -3,23 +3,17 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform player;
+    public float cameraDistance = 30.0f;
+    public float offset = 0;
 
-    public GameObject player;        //Public variable to store a reference to the player game object
-
-
-    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
-
-    // Use this for initialization
-    void Start()
+    void Awake()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
     }
 
-    // LateUpdate is called after Update each frame
-    void LateUpdate()
+    private void Update()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        transform.position = new Vector3(transform.position.x, player.position.y + offset, transform.position.z);
     }
 }
