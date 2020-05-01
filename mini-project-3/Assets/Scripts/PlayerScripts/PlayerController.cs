b33dd55ef1513e0ca1906a4ebbd050079a1fb87e
NicolasRobertson <robe1606@morris.umn.edu>
2020-04-30 20:21:18 -0500

    Implement Motion and Touch Controls
    Successfully implement motion and touch controls

--- mini-project-3/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ mini-project-3/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -4,28 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    //Establish downward momentum variables
+    private float currentFallSpeed;
     public float minFallSpeed = -5.0f;
     public float maxFallSpeed = -20.0f;
-    private float currentFallSpeed;
-    public float strafeSpeed = 5.0f;
     public float downwardAcceleration = 5.0f;
-    public float horizontalDeceleration = 130.0f;
-    public float horizontalAcceleration = 2.0f;
 
-    public Animator animator;
+    //Establish horizontal movement variables
+    public float sensitivity = 30;
+    public float smooth = 0.4f;
+    private Vector3 currentAcceleration, initialAcceleration;
+
     public bool isSpeedingUp;
 
+    //Establish overall player movement variables
     private Vector2 velocity;
-
     private CharacterController controller;
+
+    public Animator animator;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
         currentFallSpeed = minFallSpeed;
         isSpeedingUp = false;
-        print("Start");
+
+        initialAcceleration = Input.acceleration;
+        currentAcceleration = Vector3.zero;
+
+        transform.Translate(0, 0, 0);
     }
 
 
@@ -33,43 +40,39 @@ public class PlayerController : MonoBehaviour
     {
         float moveInput = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        //Detect tap to change fall speed
+        if (Input.touchCount > 0)
         {
-            if (currentFallSpeed == minFallSpeed)
+            if(Input.GetTouch(0).phase == TouchPhase.Began)
             {
-                currentFallSpeed = maxFallSpeed;
-                isSpeedingUp = true;
-                animator.SetBool("isSpeedingUp", isSpeedingUp);
-            }
-            else
-            {
-                currentFallSpeed = minFallSpeed;
-                isSpeedingUp = false;
-                animator.SetBool("isSpeedingUp", isSpeedingUp);
+                changeSpeed(isSpeedingUp);
             }
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            velocity.x--;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            velocity.x++;
-        }
+        //Set x and y velocities
+        currentAcceleration = Vector3.Lerp(currentAcceleration, Input.acceleration - initialAcceleration, Time.deltaTime/smooth);
 
-        if (moveInput != 0)
+        velocity.x = Mathf.Clamp(currentAcceleration.x * sensitivity, -100, 100);
+        velocity.y = Mathf.MoveTowards(velocity.y, currentFallSpeed, downwardAcceleration * Time.deltaTime);
+
+        //Move the player
+        transform.Translate(velocity * Time.deltaTime);
+    }
+
+    public void changeSpeed(bool speedUp)
+    {
+        if (speedUp == false)
         {
-            velocity.x = Mathf.MoveTowards(velocity.x, strafeSpeed, horizontalAcceleration * Time.deltaTime);
-            velocity.y = Mathf.MoveTowards(velocity.y, currentFallSpeed, downwardAcceleration * Time.deltaTime);
+            currentFallSpeed = maxFallSpeed;
+            isSpeedingUp = true;
+            animator.SetBool("isSpeedingUp", isSpeedingUp);
         }
         else
         {
-            velocity.x = Mathf.MoveTowards(velocity.x, 0, horizontalDeceleration * Time.deltaTime);
-            velocity.y = Mathf.MoveTowards(velocity.y, currentFallSpeed, downwardAcceleration * Time.deltaTime);
+            currentFallSpeed = minFallSpeed;
+            isSpeedingUp = false;
+            animator.SetBool("isSpeedingUp", isSpeedingUp);
         }
-
-        transform.Translate(velocity * Time.deltaTime);
     }
 
 }
